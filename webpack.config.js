9d29c8f2a9f3d57f510e83445436cbca1423995e
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-12-03 19:46:29 +0100

    Some updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,13 @@ const
   UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
   TerserPlugin = require('terser-webpack-plugin'),
   CompressionPlugin = require('compression-webpack-plugin'),
-  path = require('path')
+  path = require('path'),
+
+  libraryTargetMap = {
+    cjs: 'commonjs2',
+    umd: 'umd',
+    esm: 'commonjs-module'
+  }
 
 module.exports = [
   createConfig('cjs', 'development'),
@@ -27,6 +33,8 @@ function createConfig(moduleType, mode) {
     mode,
 
     output: {
+      library:  'jsElements',
+      libraryTarget:  libraryTargetMap[moduleType],
       path: path.resolve(__dirname, 'dist'),
       filename: `js-elements.${moduleType}.${mode}.js`
     },
