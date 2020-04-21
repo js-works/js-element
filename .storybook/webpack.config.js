aab713f0d2af327fddaefbc0cf1f491ec536bf5b
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-04-21 10:38:26 +0200

    Some updates

--- .storybook/webpack.config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-module.exports = async ({ config }) => {
-  //if (process.env.DISABLE_HMR === 'true') {
-    //config.entry = config.entry.filter(singleEntry => !singleEntry.includes('webpack-hot-middleware'))
-  //}
-
-  return config
-}
