b2554b084ef7e09c511f3885bdcddad3aa61f18e
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-05-03 21:25:45 +0200

    Updated logic of useState and useValue

--- src/main/api/useState.js
+++ src/main/api/useState.js
@@ -2,10 +2,39 @@
 import hook from './hook'
 import globals from '../internal/globals'
 export default hook('useState', initialState => {
+  let
+    nextState,
+    mergeNecessary = false
+
   const
     c = globals.currentComponent,
     state = { ...initialState },
 
+    setState = (arg1, arg2) => {
+      mergeNecessary = true
+
+      if (typeof arg1 === 'string') {
+        nextState[arg1] =
+          typeof arg2 === 'function'
+            ? arg2(nextState[arg1])
+            : arg2
+      } else if (typeof arg1 === 'function') {
+        Object.assign(nextState, arg1(nextState))
+      } else {
+        Object.assign(nextState, arg1)
+      }
+
+      c._update(() => {
+        if (mergeNecessary) {
+          Object.assign(state, nextState)
+          mergeNecessary = false
+        }
+      })
+    }
+
+  nextState = { ...state }
+
+  /*
     setState = (arg1, arg2) => {
       let updater
 
@@ -26,6 +55,7 @@ export default hook('useState', initialState => {
         )
       })
     }
+    */
 
   return [state, setState]
 })
--- src/main/api/useValue.js
+++ src/main/api/useValue.js
@@ -1,15 +1,29 @@
 import hook from './hook'
 import globals from '../internal/globals'
 export default hook('useValue', initialValue => {
+  let nextValue = initialValue
+  
   const
     c = globals.currentComponent,
     value = { value: initialValue },
-
+  
+/*
     setValue = updater => {
       c._update(() => {
         value.value = typeof updater === 'function'
           ? updater(value.value)
           : updater
+      })
+    }
+*/
+
+    setValue = updater => {
+      nextValue = typeof updater === 'function'
+        ? updater(nextValue)
+        : updater
+
+      c._update(() => {
+        value.value = nextValue
       })
     }
 
