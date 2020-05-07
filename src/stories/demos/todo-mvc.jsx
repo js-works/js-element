a906f456a2972b53cf402476808f88759d5cf024
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-05-07 21:59:28 +0200

    Changed syntax in TodoMVC demo

--- src/stories/demos/todo-mvc.jsx
+++ src/stories/demos/todo-mvc.jsx
@@ -46,50 +46,72 @@ const Item = defineElement({
 
   props: {
     todo: prop.obj.req()
-  }
-}, (c, props) => {
-  const
-    root = getRoot(c),
+  },
 
-    [state, setState] = useState(c, {
-      active: false,
-      title: props.todo.title
-    }),
+  init(c, props) {
+    const
+      root = getRoot(c),
 
-    onToggle = ev => {
-      root.dispatchEvent(
-        new CustomEvent('todo.toggle', {
-          bubbles: true,
-          detail: { id: props.todo.id, completed: ev.target.checked },
-        })
-      )
-    },
-  
-    onDestroy = () => {
-      root.dispatchEvent(
-        new CustomEvent('todo.destroy', {
-          bubbles: true,
-          detail: { id: props.todo.id }
-        })
-      )
-    },
+      [state, setState] = useState(c, {
+        active: false,
+        title: props.todo.title
+      }),
 
-    onDoubleClick = ev => {
-      setState({ active: true })
-      ev.target.parentElement.nextSibling.focus()
-    },
+      onToggle = ev => {
+        root.dispatchEvent(
+          new CustomEvent('todo.toggle', {
+            bubbles: true,
+            detail: { id: props.todo.id, completed: ev.target.checked },
+          })
+        )
+      },
+    
+      onDestroy = () => {
+        root.dispatchEvent(
+          new CustomEvent('todo.destroy', {
+            bubbles: true,
+            detail: { id: props.todo.id }
+          })
+        )
+      },
 
-    onInput = ev => {
-      setState({ title: ev.target.value })
-    },
+      onDoubleClick = ev => {
+        setState({ active: true })
+        ev.target.parentElement.nextSibling.focus()
+      },
 
-    onKeyDown = ev => {
-      if (ev.keyCode === ENTER_KEY || ev.keyCode === ESC_KEY) {
-        setState({
-          active: false,
-          title: state.title.trim()
-        })
+      onInput = ev => {
+        setState({ title: ev.target.value })
+      },
 
+      onKeyDown = ev => {
+        if (ev.keyCode === ENTER_KEY || ev.keyCode === ESC_KEY) {
+          setState({
+            active: false,
+            title: state.title.trim()
+          })
+
+          if (state.title) {
+            root.dispatchEvent(
+              new CustomEvent('todo.edit', {
+                bubbles: true,
+                detail: { id: props.todo.id, title: state.title }
+              })
+            )
+          } else {
+            root.dispatchEvent(
+              new CustomEvent('todo.destroy', {
+                bubbles: true,
+                detail: { id: props.todo.id },
+              }),
+            )
+          }
+        }
+      },
+
+      onBlur = () => {
+        setState({ active: false })
+          
         if (state.title) {
           root.dispatchEvent(
             new CustomEvent('todo.edit', {
@@ -101,70 +123,50 @@ const Item = defineElement({
           root.dispatchEvent(
             new CustomEvent('todo.destroy', {
               bubbles: true,
-              detail: { id: props.todo.id },
-            }),
+              detail: { id: props.todo.id }
+            })
           )
         }
       }
-    },
 
-    onBlur = () => {
-      setState({ active: false })
-        
-      if (state.title) {
-        root.dispatchEvent(
-          new CustomEvent('todo.edit', {
-            bubbles: true,
-            detail: { id: props.todo.id, title: state.title }
-          })
-        )
-      } else {
-        root.dispatchEvent(
-          new CustomEvent('todo.destroy', {
-            bubbles: true,
-            detail: { id: props.todo.id }
-          })
-        )
+    return () => {
+      const classes = []
+
+      if (state.active) {
+        classes.push('editing')
       }
-    }
 
-  return () => {
-    const classes = []
+      if (props.todo.completed) {
+        classes.push('completed')
+      }
 
-    if (state.active) {
-      classes.push('editing')
-    }
-
-    if (props.todo.completed) {
-      classes.push('completed')
-    }
-
-    return (
-      <li class={classes.join(' ')}>
-        <div class="view">
+      return (
+        <li class={classes.join(' ')}>
+          <div class="view">
+            <input
+              class="toggle"
+              type="checkbox"
+              checked={props.todo.completed}
+              onClick={onToggle}
+            />
+            <label onDblclick={onDoubleClick}>
+              {props.todo.title}
+            </label>
+            <button
+              class="destroy"
+              onClick={onDestroy}
+            />
+          </div>
           <input
-            class="toggle"
-            type="checkbox"
-            checked={props.todo.completed}
-            onClick={onToggle}
+            class="edit"
+            value={state.title}
+            onInput={onInput}
+            onKeydown={onKeyDown}
+            onBlur={onBlur}
           />
-          <label onDblclick={onDoubleClick}>
-            {props.todo.title}
-          </label>
-          <button
-            class="destroy"
-            onClick={onDestroy}
-          />
-        </div>
-        <input
-          class="edit"
-          value={state.title}
-          onInput={onInput}
-          onKeydown={onKeyDown}
-          onBlur={onBlur}
-        />
-      </li>
-    )
+        </li>
+      )
+    }
   }
 })
 
@@ -174,42 +176,44 @@ const Main = defineElement({
   props: {
     todos: prop.obj.req(),
     filter: prop.str.req()
+  },
+
+  init(c, props) {
+    const
+      root = getRoot(c),
+      completed = props.todos.every(todo => todo.completed),
+
+      onToggleAll = () => {
+        root.dispatchEvent(
+          new CustomEvent('todo.toggleAll', {
+            bubbles: true,
+            detail: { completed: !completed }
+          })
+        )
+      }
+
+    let filteredTodos =
+      props.filter === 'active'
+        ? props.todos.filter(todo => !todo.completed)
+        : props.filter === 'completed'
+          ? props.todos.filter(todo => todo.completed)
+          : props.todos
+
+    return () =>
+      <section class="main">
+        <input
+          id="toggle-all"
+          class="toggle-all"
+          type="checkbox"
+          onClick={onToggleAll}
+          checked={completed}
+        />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          {filteredTodos.map(todo => <Item todo={todo}/>)}
+        </ul>
+      </section>
   }
-}, (c, props) => {
-  const
-    root = getRoot(c),
-    completed = props.todos.every(todo => todo.completed),
-
-    onToggleAll = () => {
-      root.dispatchEvent(
-        new CustomEvent('todo.toggleAll', {
-          bubbles: true,
-          detail: { completed: !completed }
-        })
-      )
-    }
-
-  let filteredTodos =
-    props.filter === 'active'
-      ? props.todos.filter(todo => !todo.completed)
-      : props.filter === 'completed'
-        ? props.todos.filter(todo => todo.completed)
-        : props.todos
-
-  return () =>
-    <section class="main">
-      <input
-        id="toggle-all"
-        class="toggle-all"
-        type="checkbox"
-        onClick={onToggleAll}
-        checked={completed}
-      />
-      <label for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        {filteredTodos.map(todo => <Item todo={todo}/>)}
-      </ul>
-    </section>
 })
 
 const Filters = defineElement({
@@ -217,41 +221,43 @@ const Filters = defineElement({
 
   props: {
     filter: prop.str.req()
-  }
-}, (c, props) => {
-  const
-    root = getRoot(c),
-    onActiveFilter = ev => setFilter('active', ev),
-    onCompletedFilter = ev => setFilter('completed', ev),
-    onNoFilter = ev => setFilter('', ev),
-    
-    setFilter = (filter, ev) => {
-      ev.preventDefault()
-      root.dispatchEvent(
-        new CustomEvent('todo.setFilter', {
-          bubbles: true,
-          detail: { filter }
-        }))
-    }
+  },
 
-  return () =>
-    <ul class="filters">
-      <li>
-        <a class={props.filter === '' ? 'selected' : ''} onClick={onNoFilter} href="#/">
-          All
-        </a>
-      </li>
-      <li>
-        <a class={props.filter === 'active' ? 'selected' : ''} onClick={onActiveFilter} href="#/active">
-          Active
-        </a>
-      </li>
-      <li>
-        <a class={props.filter === 'completed' ? 'selected' : ''} onClick={onCompletedFilter} href="#/completed">
-          Completed
-        </a>
-      </li>
-    </ul>
+  init(c, props) {
+    const
+      root = getRoot(c),
+      onActiveFilter = ev => setFilter('active', ev),
+      onCompletedFilter = ev => setFilter('completed', ev),
+      onNoFilter = ev => setFilter('', ev),
+      
+      setFilter = (filter, ev) => {
+        ev.preventDefault()
+        root.dispatchEvent(
+          new CustomEvent('todo.setFilter', {
+            bubbles: true,
+            detail: { filter }
+          }))
+      }
+
+    return () =>
+      <ul class="filters">
+        <li>
+          <a class={props.filter === '' ? 'selected' : ''} onClick={onNoFilter} href="#/">
+            All
+          </a>
+        </li>
+        <li>
+          <a class={props.filter === 'active' ? 'selected' : ''} onClick={onActiveFilter} href="#/active">
+            Active
+          </a>
+        </li>
+        <li>
+          <a class={props.filter === 'completed' ? 'selected' : ''} onClick={onCompletedFilter} href="#/completed">
+            Completed
+          </a>
+        </li>
+      </ul>
+  }
 })
 
 const Footer = defineElement({
@@ -260,25 +266,27 @@ const Footer = defineElement({
   props: {
     todos: prop.obj.req(),
     filter: prop.str.req()
+  },
+
+  init(c, props) {
+    const
+      root = getRoot(c),
+      completed = props.todos.filter(todo => todo.completed).length,
+      remaining = props.todos.length - completed,
+
+      onClearCompleted = () => 
+        root.dispatchEvent(
+          new CustomEvent('todo.clearCompleted', { bubbles: true }))
+
+    return () =>
+      <footer class="footer">
+        <span class="todo-count">
+          <strong>{remaining}</strong> {remaining === 1 ? 'item' : 'items'} left
+        </span>
+        <Filters filter={props.filter}/>
+        {!completed ? '' : <button class="clear-completed" onClick={onClearCompleted}>Clear completed</button>}
+      </footer>
   }
-}, (c, props) => {
-  const
-    root = getRoot(c),
-    completed = props.todos.filter(todo => todo.completed).length,
-    remaining = props.todos.length - completed,
-
-    onClearCompleted = () => 
-      root.dispatchEvent(
-        new CustomEvent('todo.clearCompleted', { bubbles: true }))
-
-  return () =>
-    <footer class="footer">
-      <span class="todo-count">
-        <strong>{remaining}</strong> {remaining === 1 ? 'item' : 'items'} left
-      </span>
-      <Filters filter={props.filter}/>
-      {!completed ? '' : <button class="clear-completed" onClick={onClearCompleted}>Clear completed</button>}
-    </footer>
 })
 
 const TodoMvc = defineElement('todo-mvc', c => {
