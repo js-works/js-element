d07097f96f9ecb7e87dee6b61a1e136d5bd232f6
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-05-24 12:19:54 +0200

    Increased version number

--- src/main/internal/platform.js
+++ src/main/internal/platform.js
@@ -1,9 +1,9 @@
 import { h as vdom_h, patch } from '../internal/vdom'
+export { vdom_h as h, vdom_isValidElement as isValidElement, vdom_render as render }
 
 //import 'preact/debug'
 //export { h, isValidElement, render } from 'preact'
 //export { h, isValidElement, render } from 'dyo'
-export { vdom_h as h, vdom_isValidElement as isValidElement, vdom_render as render }
 
 function vdom_isValidElement(it) {
   return it !== null && typeof it === 'object' && it.kind === 'virtual-element'
